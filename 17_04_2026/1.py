a62952f8f204174c8e945e02695f7712894048b7
Bakhtiiar Galimov <bexagalimov@gmail.com>
2026-04-17 14:58:08 +0500

    Fixing error

--- 17_04_2026/1.py
+++ 17_04_2026/1.py
@@ -2,7 +2,7 @@
 import sympy
 
 x = sympy.symbols('x')
-y = x*sympy.log(2, x) + sympy.sin(x)**2
+y = x*sympy.log(x, sympy.E) + sympy.sin(x)**2
 print(sympy.latex(y), '\n')
 y1 = sympy.diff(y,x,1)
 print(sympy.latex(y1), '\n')
@@ -11,7 +11,7 @@ print(sympy.latex(y2), '\n')
 y4 = sympy.diff(y,x,4)
 print(sympy.latex(y4), '\n')
 p = sympy.plotting.plot(y, (x, -10, 10), legend=1, color='b', show=False)
-p.append(sympy.plotting.plot(y1, (x, -10, 10), legend=1, color='b', show=False)[0])
-p.append(sympy.plotting.plot(y2, (x, -10, 10), legend=1, color='b', show=False)[0])
-p.append(sympy.plotting.plot(y4, (x, -10, 10), legend=1, color='b', show=False)[0])
+p.append(sympy.plotting.plot(y1, (x, -10, 10), legend=1, show=False)[0])
+p.append(sympy.plotting.plot(y2, (x, -10, 10), legend=1, show=False)[0])
+p.append(sympy.plotting.plot(y4, (x, -10, 10), legend=1, show=False)[0])
 p.show()
