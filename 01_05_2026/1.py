36d784219e7c82ef67342e05fe6838dde027b51d
Bakhtiiar Galimov <bexagalimov@gmail.com>
2026-05-01 14:47:20 +0500

    Sortings

---
+++ 01_05_2026/1.py
@@ -0,0 +1,22 @@
+from random import randrange
+
+def insertionSort(arr):
+    n = len(arr)
+    if n <= 1:
+        return
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+string = ""
+for i in range(10):
+    string += alphabet[randrange(len(alphabet))]
+print(string)
+array = list(string)
+insertionSort(array)
+print("".join(array))
